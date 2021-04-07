2e5ceee5d69e0dcc1450881753de561776742c0a
Rishab.Kashyap <rishabh.kashyap@incture.com>
2021-04-07 11:11:59 +0530

    master-data corrected

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -3,148 +3,110 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class Globals {
 
-    data={
-        InstitutionName: "",
-        InstitutionType: "",
-        TemplateType: "",
-        Colleges: "",
-        Departments: "",
-        Description:"",
-        CollegeDetail:[{
-            college: "School Of Engineering",
-            UGC_Reg_ID: "",
-            AICTE_Reg_ID: ""
-          },
+  data = {
+    InstitutionName: "",
+    InstitutionType: "",
+    TemplateType: "",
+    Colleges: "",
+    Departments: "",
+    Description: "",
+    CollegeDetail: [{
+      college: "School Of Engineering",
+      UGC_Reg_ID: "",
+      AICTE_Reg_ID: "",
+      courseTemplate: "",
+    }
+    ],
+    CollegeQuotas: [
+      {
+        name: "",
+        percent: ""
+      },
+      {
+        name: "",
+        percent: ""
+      }
+    ],
+    DeptDetail: [{
+      dept: "Photonics",
+      UGC_Reg_ID: "",
+      AICTE_Reg_ID: "",
+      courseTemplate: {
+        sub: "",
+        id: "",
+        Seats: "",
+        Quotas: [
           {
-            college: "School Of Management",
-            UGC_Reg_ID: "",
-            AICTE_Reg_ID: ""
-          },
-          {
-            college: "School Of Legal Studies",
-            UGC_Reg_ID: "",
-            AICTE_Reg_ID: ""
-          },
-          {
-            college: "College of Pure Sciences",
-            UGC_Reg_ID: "",
-            AICTE_Reg_ID: ""
-          }],
-      
-        CourseDetails:{
-      
-            CourseTemplates:[
-                {
-                  name: "",
-                  courseTemplate:[
-                                  
-                    ]
-      
-                },
-      
-                {
-                  name: "",
-                  courseTemplate:[
-                      
-                    ]
-      
-                }
-                     ]
-      
-      
-                },
-      
-        Quotas:[
-          { name: "",
-              percent: ""
-          },
-          { name: "",
+            name: "",
             percent: ""
-          }							
-                      ],
-                        
-              
-                      
-      
-        SeatMatrix:{	
-            departments:[
-                {
-                id: "",
-                coursesDegrees:[
-                      {
-                      id:"",
-                      Seats: "",
-                      Quotas:[
-                        { name: "",
-                          percent: ""
-                        },
-                        { name: "",
-                          percent: ""
-                        }
-                               ],
-              
-                      },
-                      
-                      {
-                      id:"",
-                      Seats: "",
-                      Quotas:[
-                        { name: "",
-                          percent: ""
-                        },
-                        { name: "",
-                          percent: ""
-                        }
-                               ],
-              
-                      }
-                    ]
-                },
-      
-                {
-                id: "",
-                coursesDegrees:[
-                      {
-                      id:"",
-                      Seats: "",
-                      Quotas:[
-                        { name: "",
-                          percent: ""
-                        },
-                        { name: "",
-                          percent: ""
-                        }
-                               ],
-              
-                      },
-                                      {
-                      id:"",
-                      Seats: "",
-                      Quotas:[
-                        { name: "",
-                          percent: ""
-                        },
-                        { name: "",
-                          percent: ""
-                        }
-                               ],
-              
-                      }
-                    ]
-                }
-            
-                 ]				
-      
-              },
-      
-        Verification:{
-            documents:[]
-            
-               },
-      
-        AnnouncementFaq:{
-                               
-                              }
-      
-      };
+          },
+          {
+            name: "",
+            percent: ""
+          }
+        ]
+      }
+    },
+    {
+      dept: "Environmental Studies",
+      UGC_Reg_ID: "",
+      AICTE_Reg_ID: "",
+      courseTemplate: {
+        sub: "",
+        id: "",
+        Seats: "",
+        Quotas: [
+          {
+            name: "",
+            percent: ""
+          },
+          {
+            name: "",
+            percent: ""
+          }
+        ]
+      }
+    }
+    ],
+    CourseTemplates: [
+      {
+        name: "",
+        courseTemplate: [
+          {
+            sub: "",
+            id: "",
+            Seats: ""
+           
+          },
+          {
+            sub: "",
+            id: "",
+            Seats: ""
+          }
+
+        ]
+
+      },
+
+      {
+        name: "",
+        courseTemplate: [
+          {},
+          {},
+          {}
+        ]
+
+      }
+    ],
+
+    Verification: {
+      documents: []
+
+    },
+
+    AnnouncementFaq: {
+
+    }
+
+  };
 }
